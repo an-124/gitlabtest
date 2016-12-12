6c6c32ecbf2084942e393ccc0417598243661046
an-124 <sir.rusl98@mail.ru>
2016-12-12 15:55:51 +0300

    codex

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prot1
+{
+    public partial class Form1 : Form
+    {
+        public Form3 f3 = new Form3();
+        public Form2 f2 = new Form2(); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+            f2.ShowDialog();
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            f3.ShowDialog();
+            Close();
+        }
+    }
+}
